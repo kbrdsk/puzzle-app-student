eeaccd4619cf0fb3df83ecd6bb8a5b2101c0f2df
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-27 16:47:41 -0700

    added history and routes for individual puzzles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import Login from "./login/index";
 import Home from "./home/index";
+import { puzzleList } from "./puzzles/index";
+
+const history = createBrowserHistory();
+
+const renderPuzzleRoute = (puzzle) => (
+	<Route key={puzzle.id} path={`/${puzzle.id}`} component={puzzle.Page} />
+);
 
 const Routes = () => {
 	return (
-		<BrowserRouter>
+		<BrowserRouter history={history}>
 			<Switch>
+				{puzzleList.map(renderPuzzleRoute)}
 				<Route path="/login" component={Login} />
 				<Route path="/" component={Home} />
 			</Switch>
