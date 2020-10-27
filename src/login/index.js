dfb5a9470104067cf89f5693de6cc24b4a8a89a2
Kabirdas Henry <kbrdsk@gmail.com>
2020-10-26 18:47:35 -0700

    login working

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import jwt from "jsonwebtoken";
 
-function Login() {
-	return <div>Login Window</div>;
+export default function Login() {
+	const [first, setFirst] = useState();
+	const [last, setLast] = useState();
+
+	const updateName = (updater, event) => {
+		updater(event.target.value);
+	};
+
+	const login = async (create) => {
+		const studentData = { first, last };
+		const uri =
+			`${process.env.REACT_APP_API_URI}/students` +
+			(create ? "" : "/login");
+		console.log(uri);
+		const response = await fetch(uri, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(studentData),
+		});
+		if (response.ok) {
+			const token = (await response.json()).token;
+			console.log(jwt.decode(token).student);
+		} else console.log("HTTP error, status = " + response.status);
+	};
+
+	return (
+		<div id="login-container">
+			<div className="name-container">
+				<label htmlFor="firstname">First Name:</label>
+				<input
+					type="text"
+					name="firstname"
+					id="firstname"
+					onChange={updateName.bind(null, setFirst)}
+				/>
+			</div>
+			<div className="name-container">
+				<label htmlFor="lastname">Last Name:</label>
+				<input
+					type="text"
+					name="lasname"
+					id="lasname"
+					onChange={updateName.bind(null, setLast)}
+				/>
+			</div>
+			<button className="create-student" onClick={() => login(true)}>
+				Create
+			</button>
+			<button className="login-student" onClick={() => login(false)}>
+				Log-In
+			</button>
+		</div>
+	);
 }
-
-export { Login };
