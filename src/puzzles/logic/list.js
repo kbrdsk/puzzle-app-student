ced5538a2c437eabb9195ea539dbf117385f599f
kbrdsk <52506986+kbrdsk@users.noreply.github.com>
2020-11-11 10:02:32 -0800

    Create list.js

---
+++ src/puzzles/logic/list.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function List(props) {
+	const history = props.history;
+	const instanceList = props.instanceList;
+
+	return (
+		<div className="logic-puzzles page-container">
+			<div className="instance-list-container">
+				<h2>Logic Puzzles</h2>
+				<ul>
+					{instanceList
+						? instanceList.map(
+								renderInstanceListing.bind(null, history)
+						  )
+						: null}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+function renderInstanceListing(history, instance) {
+	return (
+		<li
+			key={instance}
+			onClick={() => history.push(`/logic/${instance}`)}
+		>
+			<h3>{capitalize(instance)}</h3>
+		</li>
+	);
+}
+
+function capitalize(string) {
+	return string.replace(/^./, (char) => char.toUpperCase());
+}
