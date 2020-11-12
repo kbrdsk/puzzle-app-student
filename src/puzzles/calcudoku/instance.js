1f31fb83c4850b269c97bd134921a5f8017c7154
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-12 15:39:27 -0800

    linting

--- src/puzzles/calcudoku/instance.js
+++ src/puzzles/calcudoku/instance.js
@@ -41,7 +41,7 @@ export default function Instance(props) {
 				}
 			} else console.log("HTTP error, status = " + response.status);
 		})();
-	}, [dburl, token]);
+	}, [dburl, token, sessionDataKey]);
 
 	useEffect(() => {
 		const url = `${process.env.REACT_APP_API_URL}/activepuzzle`;
