47f1de709d4a16a9c8b7eed420941dcc67895928
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-20 10:51:25 -0800

    repo now determined based on arg passed to script

--- deploy.js
+++ deploy.js
@@ -1,5 +1,15 @@
 const ghpages = require("gh-pages");
 
-ghpages.publish("build", {
-	repo: "git@github.com:kbrdsk/puzzle-app-student.git",
-});
+const args = process.argv.slice(2);
+const repo =
+	args[0] === "production"
+		? "git@github.com-mcubmath:mcubmath/puzzles.git"
+		: "git@github.com:kbrdsk/puzzle-app-student.git";
+
+ghpages.publish(
+	"build",
+	{
+		repo,
+	},
+	(err) => (err ? console.log(err) : console.log(`Published to ${repo}`))
+);
