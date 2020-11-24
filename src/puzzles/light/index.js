3a06382fb3a1eeb5abfecf78e49407dd307b42e2
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-23 19:21:15 -0800

    sorted instance list

--- src/puzzles/light/index.js
+++ src/puzzles/light/index.js
@@ -8,6 +8,14 @@ function ListPreview() {
 	return <div className="list-preview">Find all light puzzles here</div>;
 }
 
-const instanceSort = () => 1;
+function instanceSort({ instance: a }, { instance: b }) {
+	const [, levelA, numberA] = a.match(/^([a-z]+)(\d+)$/);
+	const [, levelB, numberB] = b.match(/^([a-z]+)(\d+)$/);
+	if (levelA === "beginner" && ["intermediate", "expert"].includes(levelB))
+		return -1;
+	if (levelA === "intermediate" && levelB === "expert") return -1;
+	if (levelA === levelB && numberA < numberB) return -1;
+	return 1;
+}
 
 export { name, id, ListPreview, instanceSort, Instance };
