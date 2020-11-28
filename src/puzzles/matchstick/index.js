4a9080fce5c5857649985bdd31a6a6a22f241bf0
Kabirdas Henry <kbrdsk@gmail.com>
2020-11-27 19:01:39 -0800

    added more matchstick puzzles

--- src/puzzles/matchstick/index.js
+++ src/puzzles/matchstick/index.js
@@ -8,6 +8,14 @@ function ListPreview() {
 	return <div className="list-preview">Find all matchtick puzzles here</div>;
 }
 
-const instanceList = [{ instance: "fish", title: "Fish" }];
+const instanceList = [
+	{ instance: "fish", title: "Fish" },
+	{ instance: "squares1", title: "Squares 1" },
+	{ instance: "squares2", title: "Squares 2" },
+	{ instance: "squares3", title: "Squares 3" },
+	{ instance: "donkey", title: "Donkey" },
+	{ instance: "squares4", title: "Squares 4" },
+	{ instance: "squares5", title: "Squares 5" },
+];
 
 export { name, id, ListPreview, Instance, instanceList };
