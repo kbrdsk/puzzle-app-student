2f674e7df2a0bf9c51ae34333c0f28a97ec770ff
Kabirdas Henry <kbrdsk@gmail.com>
2020-12-01 15:09:53 -0800

    removed unecessary wrappers since we're no longer fetching instance lists

--- src/puzzles/instance-list.js
+++ src/puzzles/instance-list.js
@@ -12,21 +12,11 @@ export default function List({ history, instanceList, title, puzzleType }) {
 		);
 	};
 	return (
-		<div className="calcudoku-page-container">
-			<div className="instance-list-container">
-				{instanceList.length === 0 ? (
-					<h2 className="fetching-indicator">Loading...</h2>
-				) : (
-					<div>
-						<h2>{title}</h2>
-						<ul>
-							{instanceList.map(
-								renderInstanceListing.bind(null, history)
-							)}
-						</ul>
-					</div>
-				)}
-			</div>
+		<div className="instance-list-container">
+			<h2>{title}</h2>
+			<ul>
+				{instanceList.map(renderInstanceListing.bind(null, history))}
+			</ul>
 		</div>
 	);
 }
