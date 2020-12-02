4c62ca7b05aae0616623a66630065b347f453dcd
Kabirdas Henry <kbrdsk@gmail.com>
2020-12-01 17:27:38 -0800

    changed light instance structure

--- src/puzzles/light/index.js
+++ src/puzzles/light/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Instance from "./instance";
+import List from "./instance-list";
 
 const name = "Light Puzzles";
 const id = "light";
@@ -9,19 +9,11 @@ function ListPreview() {
 }
 
 const instanceList = [
-	{ instance: "beginner1", title: "Beginner 1" },
-	{ instance: "beginner2", title: "Beginner 2" },
-	{ instance: "beginner3", title: "Beginner 3" },
-	{ instance: "beginner4", title: "Beginner 4" },
-	{ instance: "beginner5", title: "Beginner 5" },
-	{ instance: "intermediate1", title: "Intermediate 1" },
-	{ instance: "intermediate2", title: "Intermediate 2" },
-	{ instance: "intermediate3", title: "Intermediate 3" },
-	{ instance: "intermediate4", title: "Intermediate 4" },
-	{ instance: "intermediate5", title: "Intermediate 5" },
-	{ instance: "expert1", title: "Expert 1" },
-	{ instance: "expert2", title: "Expert 2" },
-	{ instance: "expert3", title: "Expert 3" },
+	{ instance: "beginner", title: "Beginner" },
+	{ instance: "intermediate", title: "Intermediate" },
+	{ instance: "expert", title: "Expert" },
 ];
 
-export { name, id, ListPreview, Instance, instanceList };
+const Instance = List;
+
+export { name, id, ListPreview, instanceList, Instance };
---
+++ src/puzzles/light/instance-list.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import Instance from "./instance";
+import { Route, Switch } from "react-router-dom";
+
+const instances = {
+	beginner: Array.from({ length: 5 }, (...[, i]) => i + 1),
+	intermediate: Array.from({ length: 5 }, (...[, i]) => i + 1),
+	expert: Array.from({ length: 3 }, (...[, i]) => i + 1),
+};
+
+export default function List({ history, name, match }) {
+	const instanceList = instances[name];
+	const [current, setCurrent] = useState(1);
+
+	useEffect(() => {
+		history.push(`/light/${name}/${current}`);
+	}, [current, name, history]);
+
+	const renderInstanceRoute = (match, instance) => {
+		return (
+			<Route
+				key={instance}
+				path={`${match.url}/${instance}`}
+				render={(props) => (
+					<Instance {...props} name={`${name}${instance}`} />
+				)}
+			/>
+		);
+	};
+
+	const renderInstanceSelector = (instance) => (
+		<button
+			key={`selector-${instance}`}
+			onClick={() => setCurrent(instance)}
+			className={`instance selector ${
+				instance === current ? "selected" : ""
+			}`}
+		></button>
+	);
+
+	const next = () => setCurrent(Math.min(current + 1, instanceList.length));
+
+	const previous = () => setCurrent(Math.max(current - 1, 1));
+
+	return (
+		<div className="light category-container">
+			<h2>{`Light Puzzles - ${capitalize(name)} `}</h2>
+			<div className="selector-container">
+				<button
+					className={`previous selector  ${
+						current === 0 ? "inactive" : "active"
+					}`}
+					onClick={previous}
+				>
+					Previous
+				</button>
+				{instanceList.map(renderInstanceSelector)}
+				<button
+					className={`next selector  ${
+						current === instanceList.length ? "inactive" : "active"
+					}`}
+					onClick={next}
+				>
+					Next
+				</button>
+			</div>
+			<Switch>
+				{instanceList.map(renderInstanceRoute.bind(null, match))}
+				<Route render={() => history.push("/light")} />
+			</Switch>
+		</div>
+	);
+}
+
+function capitalize(string) {
+	return string.replace(/^./, (char) => char.toUpperCase());
+}
