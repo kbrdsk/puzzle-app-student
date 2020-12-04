af2705c21a491ab7db5c322e9a47cd3321fdb995
Kabirdas Henry <kbrdsk@gmail.com>
2020-12-04 14:08:21 -0800

    added additional calcudokus

--- src/puzzles/calcudoku/index.js
+++ src/puzzles/calcudoku/index.js
@@ -15,10 +15,18 @@ const instanceList = [
 	{ instance: "4x4beginner1", title: "4x4 Beginner 1" },
 	{ instance: "4x4beginner2", title: "4x4 Beginner 2" },
 	{ instance: "4x4beginner3", title: "4x4 Beginner 3" },
+	{ instance: "4x4beginner4", title: "4x4 Beginner 4" },
+	{ instance: "4x4beginner5", title: "4x4 Beginner 5" },
 	{ instance: "4x4intermediate1", title: "4x4 Intermediate 1" },
 	{ instance: "4x4intermediate2", title: "4x4 Intermediate 2" },
+	{ instance: "4x4intermediate3", title: "4x4 Intermediate 3" },
+	{ instance: "4x4intermediate4", title: "4x4 Intermediate 4" },
 	{ instance: "4x4expert1", title: "4x4 Expert 1" },
+	{ instance: "4x4expert2", title: "4x4 Expert 2" },
+	{ instance: "4x4expert3", title: "4x4 Expert 3" },
 	{ instance: "5x5beginner1", title: "5x5 Beginner 1" },
+	{ instance: "5x5beginner2", title: "5x5 Beginner 2" },
+	{ instance: "5x5intermediate1", title: "5x5 Intermediate 1" },
 ];
 
 instanceList.fetchCompletionStatus = fetchUserData;
