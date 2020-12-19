959caf95c339f9b5e844073a449fa57ae8a690a9
Kabirdas Henry <kbrdsk@gmail.com>
2020-12-18 20:44:46 -0800

    hid tangrams

--- src/puzzles/index.js
+++ src/puzzles/index.js
@@ -5,7 +5,7 @@ import * as light from "./light/index";
 import * as matchstick from "./matchstick/index";
 import * as tangram from "./tangram/index";
 
-export const puzzleList = [logic, calcudoku, light, matchstick, tangram];
+export const puzzleList = [logic, calcudoku, light, matchstick, /*tangram*/];
 
 export const potd = {
 	uri: "/light/instructions",
